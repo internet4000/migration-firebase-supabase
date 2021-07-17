16d73fc89e4eb697d93b0755359c93bd4e02ec16
hugur <hugu.r.p+hello@pm.me>
2021-07-17 16:14:56 +0200

    read & serialize a local database export

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Migration script')
+import {getDatabase} from './src/database.js'
+
+const main = async () => {
+	console.log('Migration running.')
+	const dbf = await getDatabase()
+	console.log('channels, %s', dbf.channels.length)
+	console.log('tracks, %s', dbf.tracks.length)
+}
+
+main()
---
+++ src/database.js
@@ -0,0 +1,54 @@
+import {
+	readFile,
+} from 'fs/promises'
+
+const readDatabase = async () => {
+	let dataUtf8
+	try {
+		dataUtf8 = await readFile('./input/database.json', 'utf8')
+	} catch (err) {
+		console.error(err)
+	}
+
+	const dataJson = JSON.parse(dataUtf8)
+	return dataJson
+}
+
+/* takes a dict, return an array of models */
+const serializeCollection = (collection) => {
+	return Object.keys(collection).map(modelId => {
+		return collection[modelId]
+	})
+}
+
+/* models serializers */
+const serializeChannels = (collectionDict) => {
+	return serializeCollection(collectionDict)
+}
+
+const serializeTracks = (collectionDict) => {
+	return serializeCollection(collectionDict)
+}
+
+/* find the right serializer for each model */
+const serializeDatabase = (rawDb) => {
+	const models = {
+		channels: serializeChannels,
+		tracks: serializeTracks
+	}
+	const serializedDb = {}
+	Object.keys(models).forEach(modelName => {
+		const modelSerializer = models[modelName]
+		serializedDb[modelName] = modelSerializer(rawDb[modelName])
+	})
+
+	return serializedDb
+}
+
+const getDatabase = async () => {
+	const rawDb = await readDatabase()
+	const db = serializeDatabase(rawDb)
+	return db
+}
+
+export {getDatabase}
